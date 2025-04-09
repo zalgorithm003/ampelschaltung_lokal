b3119edc423ce6684821556112b374d78a5c7a4a
raspi_zmm <malikgdesigns@pm.me>
2025-04-10 00:28:45 +0200

    minor bugfixing

--- Revised/ampel_projekt_revised.py
+++ Revised/ampel_projekt_revised.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import csv
 import threading
 
-now = datetime.now()
 motion_detected = "False"
 stop_event = threading.Event()
 
@@ -148,7 +147,7 @@ def data_log_loop():
         with open("traffic_data.csv", mode="a") as file:
             writer = csv.writer(file)
             # Log date, time, motion status, and train distance
-            writer.writerow(["Date + Time:", now, "Traffic detected:", motion_detected, "Train distance:", distance, "cm"])    
+            writer.writerow(["Date + Time:", datetime.now, "Traffic detected:", motion_detected, "Train distance:", distance, "cm"])    
         sleep(1)  # Log data every second
 
 # Create threads for both loops (Main loop and Data logging loop)
